e65692fc39420b08d05bdbc2fff1834e27ce26fa
Matt Coffin <mcoffin@irys.com>
2013-02-04 18:14:37 -0700

    Revert "Invert Compressor Switch"
    This reverts commit 853018219bf43876d7a87d6e9edaadda2d76c70f.

--- src/com/frc2013/rmr662/pneumatic/PneumaticSystem.java
+++ src/com/frc2013/rmr662/pneumatic/PneumaticSystem.java
@@ -26,7 +26,7 @@ public class PneumaticSystem extends Component {
     public PneumaticSystem() {
         super();
         compressor = HardwarePool.getInstance().getCompressor(PRESSURE_SWITCH_CHANNEL, RELAY_CHANNEL);
-        disableSwitch = HardwarePool.getInstance().getDigitalInput(6, true);
+        disableSwitch = HardwarePool.getInstance().getDigitalInput(6, false);
     }
     
     protected void onBegin() {
