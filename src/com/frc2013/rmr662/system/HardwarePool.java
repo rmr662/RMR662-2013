ce2bd6fdd8eef9c067b3b6dd20b140f153544426
Drive Station Laptop <danielmercer1@gmail.com>
2013-02-08 18:39:29 -0700

    Formatted HardwarePool

--- src/com/frc2013/rmr662/system/HardwarePool.java
+++ src/com/frc2013/rmr662/system/HardwarePool.java
@@ -12,12 +12,14 @@ import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Solenoid;
 
 /**
- * A pool of hardware components (anything that uses IO channels). 
+ * A pool of hardware components (anything that uses IO channels).
  */
 public class HardwarePool {
-    /** The channel number to use for unused breakouts */
+
+    /**
+     * The channel number to use for unused breakouts
+     */
     private static final int UNUSED = -1;
-    
     // Singleton stuff
     private static HardwarePool instance;
 
@@ -27,24 +29,38 @@ public class HardwarePool {
 	}
 	return instance;
     }
-    
+
     private static class HardwareElement {
-    	/** The hardware object (i.e. Jaguar, Solenoid, DigitalInput, etc.) */
-    	final Object o;
-	/** The 12C channel on the Digital Breakout */
+
+	/**
+	 * The hardware object (i.e. Jaguar, Solenoid, DigitalInput, etc.)
+	 */
+	final Object o;
+	/**
+	 * The 12C channel on the Digital Breakout
+	 */
 	final int _12C;
-	/** The PWM channel on the Digital Breakout */
+	/**
+	 * The PWM channel on the Digital Breakout
+	 */
 	final int pwm;
-	/** The Relay channel on the Digital Breakout */
+	/**
+	 * The Relay channel on the Digital Breakout
+	 */
 	final int relay;
-	/** The Digital I/O channel on the Digital Breakout */
+	/**
+	 * The Digital I/O channel on the Digital Breakout
+	 */
 	final int digital;
-	
-	/** The channel on the Analog Breakout */
+	/**
+	 * The channel on the Analog Breakout
+	 */
 	final int analog;
-	/** The channel on the Solenoid Breakout */
+	/**
+	 * The channel on the Solenoid Breakout
+	 */
 	final int solenoid;
-	
+
 	/**
 	 * @param o The hardware object (i.e. Jaguar, Solenoid, etc.)
 	 * @param digital The digital channel
@@ -56,31 +72,30 @@ public class HardwarePool {
 	 */
 	HardwareElement(Object o, int digital, int pwm, int relay, int _12C, int analog, int solenoid) {
 	    this.o = o;
-		this.digital = digital;
+	    this.digital = digital;
 	    this.pwm = pwm;
 	    this.relay = relay;
 	    this._12C = _12C;
 	    this.analog = analog;
 	    this.solenoid = solenoid;
 	}
-	
+
 	boolean matches(Class c, int digital, int pwm, int relay, int _12C, int analog, int solenoid) {
-		if (o.getClass().equals(c)) {
-			if (this.digital == digital
-					&& this.pwm == pwm
-					&& this.relay == relay
-					&& this._12C == _12C
-					&& this.analog == analog
-					&& this.solenoid == solenoid) {
-				return true;
-			} else {
-				throw new IllegalStateException();
-			}
+	    if (o.getClass().equals(c)) {
+		if (this.digital == digital
+			&& this.pwm == pwm
+			&& this.relay == relay
+			&& this._12C == _12C
+			&& this.analog == analog
+			&& this.solenoid == solenoid) {
+		    return true;
+		} else {
+		    throw new IllegalStateException();
 		}
-		return false;
+	    }
+	    return false;
 	}
     }
-    
     // Fields
     private HardwareElement[] hardwares;
     private HardwareElement lastObject;
@@ -91,15 +106,15 @@ public class HardwarePool {
     private int last12C;
     private int lastAnalog;
     private int lastSolenoid;
-    
+
     private HardwarePool() {
 	hardwares = new HardwareElement[0];
     }
-    
+
     private boolean exists(Class c, int digital, int pwm, int relay, int solenoid) {
-    	return exists(c, digital, pwm, relay, UNUSED, UNUSED, solenoid);
+	return exists(c, digital, pwm, relay, UNUSED, UNUSED, solenoid);
     }
-    
+
     private boolean exists(Class c, int digital, int pwm, int relay, int _12C, int analog, int solenoid) {
 	this.lastDigital = digital;
 	this.lastPWM = pwm;
@@ -107,34 +122,33 @@ public class HardwarePool {
 	this.last12C = _12C;
 	this.lastAnalog = analog;
 	this.lastSolenoid = solenoid;
-    	for (int i = 0; i < hardwares.length; i++) {
+	for (int i = 0; i < hardwares.length; i++) {
 	    final HardwareElement h = hardwares[i];
-		if (h.matches(c, digital, pwm, relay, _12C, analog, solenoid)) {
-	    	lastObject = h;
-	    	return true;
+	    if (h.matches(c, digital, pwm, relay, _12C, analog, solenoid)) {
+		lastObject = h;
+		return true;
 	    }
 	}
 	return false;
     }
-    
-    private Object getFoundObject() {
-    	return lastObject;
-    }
-    
-    private void addObject(Object o) {
-		addToArray(new HardwareElement(o, lastDigital, lastPWM, lastRelay, last12C, lastAnalog, lastSolenoid));
-	}
-    
-    private void addToArray(HardwareElement h) {
-    	final HardwareElement[] temp = hardwares;
-    	final int length = hardwares.length;
-    	hardwares = new HardwareElement[length + 1];
-    	System.arraycopy(temp, 0, hardwares, 0, length);
-    	hardwares[length] = h;
-    }
-    
-    // ============== PUBLIC METHODS ==========================
 
+    private Object getFoundObject() {
+	return lastObject;
+    }
+
+    private void addObject(Object o) {
+	addToArray(new HardwareElement(o, lastDigital, lastPWM, lastRelay, last12C, lastAnalog, lastSolenoid));
+    }
+
+    private void addToArray(HardwareElement h) {
+	final HardwareElement[] temp = hardwares;
+	final int length = hardwares.length;
+	hardwares = new HardwareElement[length + 1];
+	System.arraycopy(temp, 0, hardwares, 0, length);
+	hardwares[length] = h;
+    }
+
+    // =============================== PUBLIC METHODS ==========================
     /**
      * Gets a Jaguar on the given channel
      *
@@ -144,65 +158,65 @@ public class HardwarePool {
      */
     public synchronized RMRJaguar getJaguar(int channel, double multiplier) {
 	if (exists(Jaguar.class, UNUSED, channel, UNUSED, UNUSED)) {
-		return new RMRJaguar((Jaguar) getFoundObject(), multiplier);
+	    return new RMRJaguar((Jaguar) getFoundObject(), multiplier);
 	} else {
-		Jaguar j = new Jaguar(channel);
-		addObject(j);
-		return new RMRJaguar(j, multiplier);
+	    Jaguar j = new Jaguar(channel);
+	    addObject(j);
+	    return new RMRJaguar(j, multiplier);
 	}
     }
 
-	/**
+    /**
      * Gets an RMRSolenoid on the given channel
      */
     public synchronized RMRSolenoid getSolenoid(int channel, boolean inverted) {
-    	if (exists(Solenoid.class, UNUSED, UNUSED, UNUSED, channel)) {
-    		return new RMRSolenoid((Solenoid) getFoundObject(), inverted);
-    	} else {
-    		RMRSolenoid c = new RMRSolenoid(channel, inverted);
-    		addObject(c);
-    		return c;
-    	}
+	if (exists(Solenoid.class, UNUSED, UNUSED, UNUSED, channel)) {
+	    return new RMRSolenoid((Solenoid) getFoundObject(), inverted);
+	} else {
+	    RMRSolenoid c = new RMRSolenoid(channel, inverted);
+	    addObject(c);
+	    return c;
+	}
     }
 
     public synchronized RMRDigitalInput getDigitalInput(int channel,
 	    boolean inverted) {
 	if (exists(Compressor.class, channel, UNUSED, UNUSED, UNUSED)) {
-		return new RMRDigitalInput((DigitalInput) getFoundObject(), inverted);
+	    return new RMRDigitalInput((DigitalInput) getFoundObject(), inverted);
 	} else {
-		RMRDigitalInput rmrDI = new RMRDigitalInput(channel, inverted);
-		addObject(rmrDI.di);
-		return rmrDI;
+	    RMRDigitalInput rmrDI = new RMRDigitalInput(channel, inverted);
+	    addObject(rmrDI.di);
+	    return rmrDI;
 	}
     }
-    
+
     public synchronized Compressor getCompressor(int digitalChannel, int relayChannel) {
 	if (exists(Compressor.class, digitalChannel, UNUSED, relayChannel, UNUSED)) {
-		return (Compressor) getFoundObject();
+	    return (Compressor) getFoundObject();
 	} else {
-		Compressor c = new Compressor(digitalChannel, relayChannel);
-		addObject(c);
-		return c;
+	    Compressor c = new Compressor(digitalChannel, relayChannel);
+	    addObject(c);
+	    return c;
 	}
     }
-    
+
     public synchronized Servo getServo(int channel) {
-    if (exists(Servo.class, UNUSED, channel, UNUSED, UNUSED)) {
-    	return (Servo) getFoundObject();
-    } else {
-    	Servo s = new Servo(channel);
-    	addObject(s);
-    	return s;
+	if (exists(Servo.class, UNUSED, channel, UNUSED, UNUSED)) {
+	    return (Servo) getFoundObject();
+	} else {
+	    Servo s = new Servo(channel);
+	    addObject(s);
+	    return s;
+	}
     }
-    }
-    
+
     public synchronized Encoder getEncoder(int channelA, int channelB) {
-    	if (exists(Encoder.class, channelA, UNUSED, UNUSED, UNUSED)) {
-    		return (Encoder) getFoundObject();
-    	} else {
-    		Encoder e = new Encoder(channelA, channelB);
-    		addObject(e);
-    		return e;
-    	}
+	if (exists(Encoder.class, channelA, UNUSED, UNUSED, UNUSED)) {
+	    return (Encoder) getFoundObject();
+	} else {
+	    Encoder e = new Encoder(channelA, channelB);
+	    addObject(e);
+	    return e;
+	}
     }
 }
