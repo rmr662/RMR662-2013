12f7025a250b5b198a49b5b6a77b64e1522a68a2
Matt Coffin <mcoffin@irys.com>
2013-02-16 11:40:53 -0700

    Add servo code to matts stuff

--- src/com/frc2013/rmr662/manipulator/Foot.java
+++ src/com/frc2013/rmr662/manipulator/Foot.java
@@ -5,9 +5,10 @@
 package com.frc2013.rmr662.manipulator;
 
 import com.frc2013.rmr662.system.generic.Component;
+import com.frc2013.rmr662.wrappers.RMRJaguar;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Servo;
 
 /**
  *
@@ -17,14 +18,42 @@ public class Foot extends Component {
     
     private DoubleSolenoid footSolenoid;
     private Joystick controller;
+    private Carriage carriage;
     
-    private Jaguar motor;
+    private class Carriage extends RMRJaguar {
+        
+        private Servo lockingServo;
+        
+        private static final double SERVO_LOCK = 0.0; // TODO
+        private static final double SERVO_UNLOCK = SERVO_LOCK + 0.6;
+        
+        public Carriage(int channel, double multiplier) {
+            super(channel, multiplier);
+            lockingServo = new Servo(4);
+        }
+        
+        public void set(double speed) {
+            
+            if (speed > 0 || controller.getRawButton(2) || Math.abs(speed) < 0.05) {
+                lockingServo.set(SERVO_LOCK);
+            } else {
+                lockingServo.set(SERVO_UNLOCK);
+            }
+            
+            super.set(speed);
+        }
+        
+        public void free() {
+            lockingServo.free();
+            super.free();
+        }
+        
+    }
     
     public Foot() {
 	footSolenoid = new DoubleSolenoid(4, 5);
 	controller = new Joystick(3);
-	
-	motor = new Jaguar(3);
+        carriage = new Carriage(3, 1.0);
     }
     
     protected void update() {
@@ -35,12 +64,12 @@ public class Foot extends Component {
 	}
 	
 	double speed = controller.getRawAxis(3);
-	motor.set(speed);
+        carriage.set(speed);
     }
     
     protected void onEnd() {
 	footSolenoid.free();
-	motor.free();
+        carriage.free();
     }
     
     
